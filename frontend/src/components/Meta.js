7f682557202e0f2e4f71f95e46bc96fd04a848ea
Vatsal Verma <vatsalverma999@gmail.com>
2021-08-25 18:45:19 +0530

    First commit

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Helmet } from 'react-helmet'
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name='description' content={description} />
+      <meta name='keyword' content={keywords} />
+    </Helmet>
+  )
+}
+
+Meta.defaultProps = {
+  title: 'Automotive Addicts',
+  description: 'Car review and buying website',
+  keywords: 'car, bike, tuning',
+}
+
+export default Meta
